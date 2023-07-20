10a11d9b02117630cd9c82797e4d79b21ae8b6ea
Collins <collinskiarie13@outlook.com>
2023-07-20 10:16:24 +0300

    made a self closing tag for the image element

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function About({image='https://via.placeholder.com/215', about}) {
     return <aside>
-        <img src={image} alt ='blog logo'></img>
+        <img src={image} alt ='blog logo'/>
         <p>{about}</p>
     </aside>
 }
